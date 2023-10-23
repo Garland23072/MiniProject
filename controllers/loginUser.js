9b33dac7d7167a252018d813e0902767f107c11b
Garland23072 <uaungkoon10413@gmail.com>
2023-10-23 18:49:39 +0700

    first commit

---
+++ controllers/loginUser.js
@@ -0,0 +1,23 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/User')
+
+module.exports = (req, res) => {
+    const { username, password } = req.body;
+    console.log("req.body login",req.body)
+    User.findOne({username: username}, (error, user) => {
+        if (user) {
+            bcrypt.compare(password, user.password, (error, same) => {
+                if (same) {
+                    // store user session, will talk about it later
+                    req.session.userId = user._id;
+                    req.session.username = user.username;
+                    res.redirect('/');
+                } else {
+                    res.redirect('/auth/login');
+                }
+            })
+        } else {
+            res.redirect('/auth/login');
+        }
+    })
+}
